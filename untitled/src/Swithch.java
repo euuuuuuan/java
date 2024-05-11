d4f6db318e0e0ae1540ad244be29237d60e93176
Euuuuuuan <euan.may24@gmail.com>
2024-05-11 18:02:43 +0900

    Switch test

---
+++ untitled/src/Swithch.java
@@ -0,0 +1,40 @@
+public class Swithch {
+    public static void main(String[] args) {
+        int day = 5;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
+        int days = 4;
+        switch (days) {
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to the Weekend");
+        }
+    }
+}
