97536aef3969b24192145aaef9263840ff65695d
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-12 23:22:15 +0900

    A Method with If...Else

--- class_method/src/Skin.java
+++ class_method/src/Skin.java
@@ -1,5 +1,15 @@
 public class Skin {
+
+    static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println("Access denied - You are not old enough!");
+
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Skin Pack");
+        checkAge(20);
     }
 }
