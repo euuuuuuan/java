8f0e580936b927ff2c4524edd23a169ec3bccf71
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-12 23:21:15 +0900

    Multiple Parameters(Name and Age)

--- class_method/src/Class.java
+++ class_method/src/Class.java
@@ -21,13 +21,23 @@ public class Class {
         myMethod("Anja");
 
     }
-*/
+*//*
     static int myTest(int x) {
         return 5 + x;
     }
 
     public static void main(String[] args) {
         System.out.println(myTest(3));
+    }
+*/
+    static void myMethod(String fname, int age) {
+        System.out.println(fname + " is " + age);
+    }
+
+    public static void main(String[] args) {
+        myMethod("Liam", 5);
+        myMethod("Jenny", 8);
+        myMethod("Anja", 31);
     }
 
     }
