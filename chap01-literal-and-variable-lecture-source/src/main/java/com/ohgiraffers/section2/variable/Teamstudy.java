d2596dc1470064136e64b595d0adf8fa25075267
DESKTOP-USETH78\80404 <euan.may24@gmail.com>
2024-05-13 18:10:22 +0900

    팀 스터디

---
+++ chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section2/variable/Teamstudy.java
@@ -0,0 +1,47 @@
+package com.ohgiraffers.section2.variable;
+
+public class Teamstudy {
+    public static void main(String[] args) {
+
+        // 앱 1번의 문제 1,2번
+        int salary, bonus;
+        salary = 4000000;
+        bonus = 500000;
+        //point = 100;
+
+        System.out.println("이번 달 당신의 급여는 " + salary + "원 입니다.");
+        System.out.println("이번 달 당신의 보너스는 " + bonus + "원 입니다.");
+        System.out.println("이번 달 당신의 총 급여는 " + (salary + bonus) + "원 입니다.");
+
+        System.out.println();
+
+        System.out.println("1번 고객에게 " + point + "포인트 지급했습니다.");
+        System.out.println("2번 고객에게 " + point + "포인트 지급했습니다.");
+        System.out.println("3번 고객에게 " + point + "포인트 지급했습니다.");
+        System.out.println("4번 고객에게 " + point + "포인트 지급했습니다.");
+        System.out.println("5번 고객에게 " + point + "포인트 지급했습니다.");
+        System.out.println("6번 고객에게 " + point + "포인트 지급했습니다.");
+        System.out.println("7번 고객에게 " + point + "포인트 지급했습니다.");
+        System.out.println("8번 고객에게 " + point + "포인트 지급했습니다.");
+        System.out.println("9번 고객에게 " + point + "포인트 지급했습니다.");
+        System.out.println("10번 고객에게 " + point + "포인트 지급했습니다.");
+
+        //앱 2번
+
+        boolean bool;
+        char ch;
+        String str;
+        byte by;
+        short sho;
+        int in;
+        long lon = 10;
+        float flo;
+        double dob;
+
+        int point = 100;
+
+        System.out.println(point);
+        System.out.println(lon);
+
+    }
+}
