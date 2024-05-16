9a0dcf4fb52575109d869c6bec85913b42d2bf38
euuuuuuan <euan.may24@gmail.com>
2024-05-16 15:40:18 +0900

    메소드와 변수 1. static 메소드와 non-static 메소드 2. 지역변수, 매개변수, 전역변수, 클래스변수

--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application1.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application1.java
@@ -2,6 +2,28 @@ package com.ohgiraffers.section01.method;
 
 public class Application1 {
     public static void main(String[] args) {
-        
+
+        System.out.println("main() 시작됨...");
+        /*
+        * 메소드 호출 방법
+        *
+        * 1. 사용준비
+        * [클래스명] [사용할이름] = new [클래스명]();
+        *
+        * 2. 사용
+        * [사용할이름].[메소드명]();*/
+
+//        Application1 app1 = new Application1();
+
+
+        System.out.println("main() 종료됨...");
     }
+    /*
+    * [접근제어자] [반환타입] [메소드명]([매개변수...]) {
+    *   // 실행할 코드
+    *
+    *   // 반환 타입이 있을경우 반환 타입에 맞게 return
+    * */
+
+
 }
