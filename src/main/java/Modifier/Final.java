4c6edd46bb91b930a400ccefbd94fb5507b6d961
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-16 23:40:31 +0900

    Modifier 1. Final 2. Abstract

--- src/main/java/Modifier/Final.java
+++ src/main/java/Modifier/Final.java
@@ -1,2 +1,13 @@
-package Modifier;public class Final {
+package Modifier;
+
+public class Final {
+    final int x = 10;
+    final double PI = 3.14;
+
+    public static void main(String[] args) {
+        Final myObj = new Final();
+//        myObj.x = 50; // will generate an error : cannot assign a value to a final variable
+//        myObj.PI = 25; // will generate an error : cannot assign a value to a final variable
+        System.out.println(myObj.x);
+    }
 }
--- src/main/java/Modifier/Main.java
+++ src/main/java/Modifier/Main.java
@@ -1,2 +1,15 @@
-package Modifier;public class Main {
+package Modifier;
+
+public abstract class Main {
+    public String fname = "John";
+    public int age = 24;
+    public abstract void study(); // abstract method
+}
+
+// Subclass (inherit from Main)
+class Student extends Main {
+    public int graduationYear = 2018;
+    public void study() { // the body of the abstract method is provided here
+        System.out.println("Studying all day long");
+    }
 }
