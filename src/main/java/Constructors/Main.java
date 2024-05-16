0e195d9e015505eeba4fa64ac8e203e68bc98c75
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-16 23:39:36 +0900

    Constructors

--- src/main/java/Constructors/Main.java
+++ src/main/java/Constructors/Main.java
@@ -1,2 +1,18 @@
-package Constructors;public class Main {
+package Constructors;
+
+public class Main {
+    int x; // Create a class attribute
+
+    // Create a class constructor for the Main class
+    public Main(int y) {
+//        x = 5; // Set the initial value for the class attribute x
+        x = y;
+        boolean isTrue = x == y;
+        System.out.println("x and y is same: " + isTrue);
+    }
+    // Constructor Parameters
+    public static void main(String[] args) {
+        Main myObj = new Main(5); // Create an object of class Main (This will call the constructor)
+        System.out.println(myObj.x); // Print the value of x
+    }
 }
