25fe6efc2269b32788583a2bcc56eca5e85a92fa
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-17 00:03:14 +0900

    Get & Set 1. Private

---
+++ src/main/java/Encapsulation/Main.java
@@ -0,0 +1,14 @@
+package Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Person myObj = new Person();
+//        myObj.name = "John"; // error
+//        System.out.println(myObj.name); //error
+        myObj.setName("John"); // Set the value of the name variable to "John"
+        System.out.println(myObj.getName());
+
+        // 프라이벗 클라스의 자료를 끌어 쓸 수 없기 때문에
+        // get과 set 캡슐화를 사용하여 끌어와 쓸 수 있다.
+    }
+}
