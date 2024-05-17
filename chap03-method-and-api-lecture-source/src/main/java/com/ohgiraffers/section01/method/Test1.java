5388b3093dbcb31489372fc5dc6313d3f3eb0ceb
euuuuuuan <euan.may24@gmail.com>
2024-05-17 12:21:25 +0900

    1. API의 사용 2. Math 메소드와 난수 사용

--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Test1.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Test1.java
@@ -1,2 +1,12 @@
-package com.ohgiraffers.section01.method;public class Test1 {
+package com.ohgiraffers.section01.method;
+
+public class Test1 {
+    int x = 12;
+
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        System.out.println("Test1의 x값 : " + (test1.x));
+    }
+
+
 }
