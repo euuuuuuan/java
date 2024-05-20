53b6a8efdac5e8b49a1cd2fa9c7f2c31b5e5823d
euuuuuuan <euan.may24@gmail.com>
2024-05-20 11:55:42 +0900

    loop class

--- chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -1,2 +1,14 @@
-package com.ohgiraffers.section02.looping;public class Application {
+package com.ohgiraffers.section02.looping;
+
+// 실험용 클래스
+public class Application {
+    public static void main(String[] args) {
+
+        A_for a_for = new A_for();
+//        a_for.testSimpleForStatement();
+//        a_for.testForExample1();
+//        a_for.testExample2();
+//        a_for.testExample3();
+        a_for.printSimpleGugudan();
+    }
 }
