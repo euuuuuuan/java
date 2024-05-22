8bfd0e784cd90f73bf749b3199a125cc520b6e6a
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-22 23:44:51 +0900

    Array 1. Reviewing

--- src/main/java/Review/Array/App1.java
+++ src/main/java/Review/Array/App1.java
@@ -1,12 +1,31 @@
 package Review.Array;
 
-public class App {
+import java.util.Scanner;
+
+public class App1 {
     public static void main(String[] args) {
 
         /*세 학생의 점수를 정수로 입력받아 합계와 평균을 실수로 구하시오.
         (A학생 : 30점, B학생 : 52점, C학생 : 65점)*/
 
-        
+        int[] score = new int[3];
+        Scanner scan = new Scanner(System.in);
+
+        int sum = 0;
+        double avg;
+
+        for(int i = 0; i < score.length; i++) {
+            System.out.println("Student number: " + (i + 1) + " / Enter the score of the student");
+            score[i] = scan.nextInt();
+        }
+
+        for(int i = 0; i < score.length; i++) {
+            sum += score[i];
+        }
+
+        avg = (double) sum / score.length;
+        System.out.println("Their sum score is : " + sum);
+        System.out.println("Their avg score is : " + avg);
 
     }
 }
--- src/main/java/Review/Array/App2.java
+++ src/main/java/Review/Array/App2.java
@@ -1,2 +1,31 @@
-package Review.Array;public class App2 {
+package Review.Array;
+
+public class App2 {
+    public static void main(String[] args) {
+
+                /*아래의 내용을 선언하여
+        `int[][] iarr = new int[4][];`
+
+        하단의 내용을 출력하는 이중 For문을 작성하시오.
+
+        1. `iarr[0][0] = 1;        iarr[0][1] = 2;        iarr[0][2] = 3;`
+        2. `iarr[1][0] = 4;        iarr[1][1] = 5;        iarr[1][2] = 6;`
+        3. `iarr[2][0] = 7;        iarr[2][1] = 8;`
+        4. `iarr[3][0] = 9;        iarr[3][1] = 10;`*/
+
+        int[][] iarr = new int[4][];
+        iarr[0] = new int[] { 1, 2, 3 };
+        iarr[1] = new int[] { 4, 5, 6 };
+        iarr[2] = new int[] { 7, 8 };
+        iarr[3] = new int[] { 9, 10 };
+
+        for(int i = 0; i < iarr.length; i++) { // 0부터 3까지 즉 4번 실행
+            for(int j = 0; j < iarr[i].length; j++) {
+                // iarr[0]번에 있는 것은 {1,2,3} 즉 3번 실행
+                // iarr[1]번은 3번 iarr[2]과 [3]번은 2번 실행.
+                System.out.println(iarr[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
