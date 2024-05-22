394e4384930a48f4b3c1be37573f52086572e695
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-22 23:46:29 +0900

    UserInput 1. Date 2. LocalDateTime, DateTimeFormatter

--- src/main/java/UserInput/Date.java
+++ src/main/java/UserInput/Date.java
@@ -1,2 +1,26 @@
-package UserInput;public class Date {
+package UserInput;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Date {
+    public static void main(String[] args) {
+//        LocalDate myObj = LocalDate.now(); // Create a date object
+//        LocalTime myObjTime = LocalTime.now();
+//        LocalDateTime myDateTime = LocalDateTime.now();
+//
+//        System.out.println(myObj);
+//        System.out.println(myObjTime);
+//        System.out.println(myDateTime);
+
+        LocalDateTime myDateObj = LocalDateTime.now();
+        System.out.println("Before formatting: " + myDateObj);
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+
+        String formattedDate = myDateObj.format(myFormatObj);
+        System.out.println("After formatting: " + formattedDate);
+    }
 }
