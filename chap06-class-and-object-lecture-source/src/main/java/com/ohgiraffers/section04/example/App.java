f002c714b7e8cf4d0f5e20293ccde6297f4acd21
euuuuuuan <euan.may24@gmail.com>
2024-05-24 18:06:51 +0900

    Constructor

---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/example/App.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.section04.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Max", 5, "골든 리트리버");
+        System.out.println(person1.getInfo());
+    }
+
+
+}
