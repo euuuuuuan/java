26d94f1df8e7211e4da7479f396925a187df598e
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-25 01:02:06 +0900

    OOP Excercise

---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section03/abstraction4/Barista.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.section03.abstraction4;
+
+public class Barista {
+    /*           - 바리스타 :
+     *       주문에 따라 커피를 만들어라(커피객체 반환)
+     *       재고량을 관리*/
+
+    public void brewingCoffee() {}
+
+    public void managingInventory() {}
+}
