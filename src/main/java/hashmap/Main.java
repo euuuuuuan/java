38d08d3e2126bc2238a4aeb7aafcbe0f8f1194ee
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-26 00:30:50 +0900

    hash 1. map 2. set

--- src/main/java/hashmap/Main.java
+++ src/main/java/hashmap/Main.java
@@ -1,2 +1,30 @@
-package hashMap;public class Main {
+package hashmap;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a HashMap object called captialCities
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        // Add keys and values (Country, City)
+        capitalCities.put("England", "London");
+        capitalCities.put("France", "Paris");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("USA", "Washington DC");
+        System.out.println(capitalCities);
+        System.out.println(capitalCities.get("France"));
+        // Print keys
+        for (String i : capitalCities.keySet()) {
+            System.out.println(i);
+        }
+        // Print values
+        for (String i : capitalCities.values()) {
+            System.out.println(i);
+        }
+        // Print keys and values
+        for (String i : capitalCities.keySet()) {
+            System.out.println("key: " + i + " value: " + capitalCities.get(i));
+        }
+    }
 }
--- src/main/java/hashset/Main.java
+++ src/main/java/hashset/Main.java
@@ -1,2 +1,41 @@
-package hashset;public class Main {
+package hashset;
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Audi");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
+        cars.contains("Mazda");
+
+        System.out.println(cars);
+
+        // Create a HashSet object called numbers
+        HashSet<Integer> numbers = new HashSet<Integer>();
+
+        // Add values to the set
+        numbers.add(4);
+        numbers.add(7);
+        numbers.add(8);
+
+        // Show which numbers between 1 and 10 are in the set
+        for (int i = 1; i <= 10; i++) {
+            if (numbers.contains(i)) {
+                System.out.println(i + " was found in the set");
+            } else {
+                System.out.println(i + " was not found in the set");
+            }
+        }
+
+    }
 }
