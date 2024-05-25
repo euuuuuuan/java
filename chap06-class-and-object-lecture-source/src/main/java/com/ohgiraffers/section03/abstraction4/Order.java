82940fc5eebc59d9d9ce7f74d8c003774bf77038
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-25 18:28:20 +0900

    OOP Excercise

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section03/abstraction4/Order.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section03/abstraction4/Order.java
@@ -2,17 +2,54 @@ package com.ohgiraffers.section03.abstraction4;
 
 public class Order {
     // 종류 옵션 수량
-    private int price; // how much
-    private int count; // how many
+    private int price = 0; // how much
+    private int count = 0; // how many
     private String size; // Tall or Grande or Venti
     private String temp; // Iced or Hot
     private String eat; // for here or take away
 
-//    Menu menu = new Menu();
+    public void price() {
+        if (price == 3000) {
+            this.price += price;
+            System.out.println("The price is: $" + price);
+        }
+        else if (price == 3500) {
+            this.price += price;
+            System.out.println("The price is: $" + price);
+        }
+        else if (price == 4000) {
+            this.price += price;
+            System.out.println("The price is: $" + price);
+        }
+        else if (price == 4500) {
+            this.price += price;
+            System.out.println("The price is: $" + price);
+        }
+        else if (price == 5000) {
+            this.price += price;
+            System.out.println("The price is: $" + price);
+        }
+        else if (price == 5500) {
+            this.price += price;
+            System.out.println("The price is: $" + price);
+        }
+        else if (price == 6000) {
+            this.price += price;
+            System.out.println("The price is: $" + price);
+        } else if (price > 6000) {
+            this.price += price;
+            System.out.println("The total price is: $" + price);
+        } else {
+            System.out.println("Error");
+        }
+    }
+    public void count() {
+        this.count += 1;
+        System.out.println("The count you've picked fow now is: " + count);
+    }
 
-    public void sort () {}
-    public void addMore () {}
-    public void quantity () {}
-
+    //    public String getInfo () {
+//        return
+//    }
 
 }
