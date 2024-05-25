28a876b94c0d06e78d6f206a32f6d17efda9f8d2
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-25 17:46:43 +0900

    OOP Excercise

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section03/abstraction4/Barista.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section03/abstraction4/Barista.java
@@ -5,7 +5,7 @@ public class Barista {
      *       주문에 따라 커피를 만들어라(커피객체 반환)
      *       재고량을 관리*/
 
-    Coffee coffee = new Coffee();
+    Order order = new Order();
 
     public void brewingCoffee() {
 //        kiosk.
