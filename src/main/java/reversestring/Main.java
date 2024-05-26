8b6bd65080d53f586a8def3cb09b207eba003490
euan.may24@gmail.com <euan.may24@gmail.com>
2024-05-26 15:46:22 +0900

    Reverse a String

--- src/main/java/reversestring/Main.java
+++ src/main/java/reversestring/Main.java
@@ -8,7 +8,7 @@ public class Main {
         for (int i = 0; i < originalStr.length(); i++) {
             reversedStr = originalStr.charAt(i) + reversedStr;
             // i = 0 i < 5
-            // H부터 불러오기 때문에 결과값은 olleh
+            // H부터 불러오면서 reversedStr로 누적시키기 때문에 결과값은 olleh
         }
         System.out.println("reversedStr: " + reversedStr);
     }
