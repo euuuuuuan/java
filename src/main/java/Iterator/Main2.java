5427e3023898827e7571038fadb8c7755fff37e3
euan <euan.may24@gmail.com>
2024-05-26 14:00:35 +0900

    Iterator

--- src/main/java/Iterator/Main2.java
+++ src/main/java/Iterator/Main2.java
@@ -1,2 +1,27 @@
-package Iterator;public class Main2 {
+package Iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(12);
+        numbers.add(8);
+        numbers.add(2);
+        numbers.add(23);
+
+        Iterator<Integer> it = numbers.iterator();
+        while(it.hasNext()) {
+            Integer i = it.next();
+            if(i < 10) {
+                it.remove();
+            }
+        }
+        System.out.println(numbers);
+    }
+    /*Trying to remove items using a for loop
+     or a for-each loop would not work correctly
+     because the collection is changing size at the same time
+     that the code is trying to loop.*/
 }
