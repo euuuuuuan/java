1784dea211ebbd7c1aa757c710b838bce02621b3
euuuuuuan <euan.may24@gmail.com>
2024-05-29 10:28:44 +0900

    Dice Excercise

--- chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section04/use3/Dicer.java
+++ chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section04/use3/Dicer.java
@@ -1,18 +1,18 @@
 package com.ohgiraffers.section04.use3;
 
 public class Dicer {
-
-    private Dice dice;
-
-    public Dicer(Dice dice) {
-        this.dice = dice;
-    }
+//
+//    private Dice dice;
+//
+//    public Dicer(Dice dice) {
+//        this.dice = dice;
+//    }
 
     public void throwDice(Dice dice) {
 
         int data = dice.getNumber();
-
-        dice.getNumber();
+        System.out.println(data);
+//        dice.getNumber();
 
     }
 
--- chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section04/use3/SuperDice.java
+++ chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section04/use3/SuperDice.java
@@ -4,11 +4,16 @@ import com.ohgiraffers.section01.polymorphism.Rabbit;
 import com.ohgiraffers.section01.polymorphism.Tiger;
 
 public class SuperDice implements Dice {
-    int min = 1;
-    int max = 10;
+    private int min;
+    private int max;
+
+    public SuperDice() {
+        this.min = 1;
+        this.max = 10;
+    }
 
     @Override
-    public int getNumber(int dice) {
+    public int getNumber() {
 
 // (Math.random()*(max-min+1)+min);
         return this.min + (int) (Math.random() * ((this.max - this.min) + 1));
