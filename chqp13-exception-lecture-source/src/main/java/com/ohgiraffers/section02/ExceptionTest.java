ddfa4f7daeb31eeb8430be63d39e3ccc106b7546
euuuuuuan <euan.may24@gmail.com>
2024-05-31 15:17:50 +0900

    exception

--- chqp13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/ExceptionTest.java
+++ chqp13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/ExceptionTest.java
@@ -1,2 +1,15 @@
-package com.ohgiraffers.section02;public class ExceptionTest {
+package com.ohgiraffers.section02;
+
+import com.ohgiraffers.section02.exception.NotEnoughMoneyException;
+
+public class ExceptionTest {
+
+    public void checkEnoughMoney(int price, int money) throws Exception {
+
+        if (money >= price) {
+            System.out.println("상품을 구입하기 위한 금액이 충분합니다!!");
+        } else {
+            throw new NotEnoughMoneyException("가지고 있는 돈보다 상품 가격이 더 비쌉니다.");
+        }
+    }
 }
