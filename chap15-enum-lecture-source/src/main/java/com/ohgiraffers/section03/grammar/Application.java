b7f0642e722aa4c07b32f8b3dfa0be9a63ffbfce
euuuuuuan <euan.may24@gmail.com>
2024-06-03 16:20:30 +0900

    grammar

--- chap15-enum-lecture-source/src/main/java/com/ohgiraffers/section03/grammar/Application.java
+++ chap15-enum-lecture-source/src/main/java/com/ohgiraffers/section03/grammar/Application.java
@@ -1,2 +1,11 @@
-package com.ohgiraffers.section03.grammar;public class Application {
+package com.ohgiraffers.section03.grammar;
+
+public class Application {
+    // Enum 문법
+    public static void main(String[] args) {
+
+        UserRole1 admin = UserRole1.ADMIN;
+        System.out.println(admin);
+        System.out.println(admin.getNameToLowerCase());
+    }
 }
