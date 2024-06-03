d5ea840632179423d5c6d01cbf5a5805ca1cb7ed
euuuuuuan <euan.may24@gmail.com>
2024-06-03 16:59:01 +0900

    first commit

--- chqp13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/exception/PriceNegativeException.java
+++
@@ -1,8 +0,0 @@
-package com.ohgiraffers.section02.exception;
-
-public class PriceNegativeException extends NegativeException {
-
-    public PriceNegativeException(String message) {
-        super(message);
-    }
-}
