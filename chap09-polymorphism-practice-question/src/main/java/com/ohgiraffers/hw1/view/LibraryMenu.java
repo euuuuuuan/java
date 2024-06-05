95963816092321599ad08888c95d190563af81d7
euuuuuuan <euan.may24@gmail.com>
2024-06-05 12:47:07 +0900

    polymorphism examples

--- chap09-polymorphism-practice-question/src/main/java/com/ohgiraffers/hw1/view/LibraryMenu.java
+++ chap09-polymorphism-practice-question/src/main/java/com/ohgiraffers/hw1/view/LibraryMenu.java
@@ -1,5 +1,28 @@
-package com.ohgiraffers.hw1.model.view;
+package com.ohgiraffers.hw1.view;
+
+import com.ohgiraffers.hw1.controller.LibraryManager;
+
+import java.util.Scanner;
 
 public class LibraryMenu {
-    private lm
+    private LibraryManager lm;
+    Scanner sc = new Scanner(System.in);
+
+    public void mainMenu() {
+        
+    }
+
+    public void selectAll() {}
+
+    public void searchBook() {}
+
+    public void rentBook() {}
+
+    public LibraryManager getLm() {
+        return lm;
+    }
+
+    public void setLm(LibraryManager lm) {
+        this.lm = lm;
+    }
 }
